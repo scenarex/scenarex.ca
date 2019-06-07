2bc813eaffd0a6ccc551451a1a763b41e4742a1a
Mackenzie Sheridan <mackenzie@scenarex.ca>
2019-06-07 12:56:13 -0400

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Scenarex Website`,
+    author: `Scenarex Inc.`,
+    description: `Public facing website for Scenarex Inc.`,
+    siteUrl: `https://scenarex.ca`,
   },
   plugins: [
     {
