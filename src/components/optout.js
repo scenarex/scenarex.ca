1dcfca34aa5ea245749fa6208d36f783087e4385
Steve Breault <steve@scenarex.ca>
2023-09-22 17:35:51 -0400

    changed headerdata implementation

--- src/components/optout.js
+++ src/components/optout.js
@@ -3,7 +3,14 @@ import { Link } from "gatsby";
 import translations from "../utils/translations.json";
 import routes from "../utils/routes.json";
 
-function ConsentPopup(lang) {
+const ConsentPopup = (props) => {
+  const data = props.headerData ? props.headerData.data : null;
+  var lang = "";
+  if (data) {
+    lang = props.headerData.lang;
+  } else {
+    lang = "en";
+  }
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
@@ -29,11 +36,11 @@ function ConsentPopup(lang) {
     showPopup && (
       <div className="bottom-0 fixed border-t w-full bg-white p-4 shadow-md text-sm">
         <div className="gdpr-popup-content container mx-auto">
-          <p>{translations.consent_1[lang.lang]}</p>
-          <p>{translations.consent_2[lang.lang]} <Link className="text-scenarexGreen" to={routes.privacy[lang.lang]}>{translations.policy[lang.lang]}</Link>.</p>
+          <p>{translations.consent_1[lang]}</p>
+          <p>{translations.consent_2[lang]} <Link className="text-scenarexGreen" to={routes.privacy[lang]}>{translations.policy[lang]}</Link>.</p>
           <div className="gdpr-popup-actions grid grid-cols-2 gap-4 w-1/2">
-            <button onClick={handleReject}>{translations.refuse[lang.lang]}</button>
-            <button onClick={handleAccept}>{translations.accept[lang.lang]}</button>
+            <button onClick={handleReject}>{translations.refuse[lang]}</button>
+            <button onClick={handleAccept}>{translations.accept[lang]}</button>
           </div>
         </div>
       </div>
